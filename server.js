470472a12729f509a806e8d3f01bca2881081a67
Sergio Andres <hello@sergioandres.io>
2016-06-03 18:22:43 +0200

    +gulp, watchify, package.json, page.js..

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req,res){
-	res.send("Hola Mundo")
+app.set('view engine', 'pug');
+
+app.use(express.static('public'))
+
+app.get('/', function(req,res){
+	res.render('index')
+})
+
+app.get('/signup', function(req,res){
+	res.render('index')
+})
+
+app.get('/signin', function(req,res){
+	res.render('index')
 })
 
 app.listen(3000, function (err){
-	if(err) return console.log("Hubo un error"), process.exit(1);
+	if(err) return console.log('Hubo un error'), process.exit(1);
 
-	console.log("Indiangram escuchando en el puerto 3000")
+	console.log('Indiangram escuchando en el puerto 3000')
 })
